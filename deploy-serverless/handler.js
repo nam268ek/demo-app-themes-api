32128dd62b00eeca01e8ef15dfd7168124c3cbc3
nam268ek <namnguyenexe@gmail.com>
2022-01-19 19:12:42 +0700

    update helmet

--- deploy-serverless/handler.js
+++ deploy-serverless/handler.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
+const helmet = require("helmet");
+
 
 const app = express();
 const cors = require("cors");
@@ -35,6 +37,7 @@ app.use('/webhook', express.raw({type: "*/*"}))
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(cors());
 app.use(express.static("public"));
 
